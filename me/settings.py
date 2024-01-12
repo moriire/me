32e0e228e1751e457cb5c91828efd456f35d97ec
Ibraheem Mobolaji Abdulsalam <ibmabdulsalam@gmail.com>
2024-01-12 15:02:24 +0100

    Update settings.py

--- me/settings.py
+++ me/settings.py
@@ -104,11 +104,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (BASE_DIR/'static/',)
+#STATICFILES_DIRS = (BASE_DIR/'static/',)
 MEDIA_URL = '/media/'            
 MEDIA_ROOT = BASE_DIR /'media'
 
-#STATIC_ROOT =  BASE_DIR /'static'
+STATIC_ROOT =  BASE_DIR /'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 LOGIN_REDIRECT_URL = "/upload/"
