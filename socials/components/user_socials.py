bf334f8e682b57fb41a6bc242f9a47326846a7d2
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2024-01-12 14:46:46 +0100

    latest commit

--- socials/components/user_socials.py
+++ socials/components/user_socials.py
@@ -1,9 +1,21 @@
-from django_unicorn.components import UnicornView
-from socials.models import Media
+from django_unicorn.components import UnicornView, QuerySetType
+from socials.models import Media, Social
 
 class UserSocialsView(UnicornView):
-    media: Media = Media.objects.none()
+    media: QuerySetType[Media] = Media()
+    link = ''
+    user = None
+
     def mount(self):
+        self.media.name = 'Linkedin'
         self.media.user = self.request.user
+
     def save_social(self):
-        self.media.save()
+        obj = self.media.save()
+        s = Social.objects.get(user = self.user)
+        s.user_social_media.add(obj)
+        self.clear()
+
+    def clear(self):
+        self.name = ''
+        self.link = ''
--- socials/models.py
+++ socials/models.py
@@ -1,13 +1,21 @@
+from collections.abc import Iterable
 from django.db import models
 from django.contrib.auth import get_user_model
 User =  get_user_model()
 class Media(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_media_link")
-    link = models.URLField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_media_link")
+    link = models.URLField(max_length=200)
     name = models.CharField(max_length=20)
     def __str__(self):
         return self.name
+    """
+    def save(self, *a, **b) -> None:
+        x = super().save(*a, **b)
+        self.user_social_media.add(x)
+    """
 
 class Social(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_social")
-    socials = models.ManyToManyField(Media, related_name="user_social_media")
+    socials = models.ManyToManyField(Media, related_name="user_social_media", blank=True)
+    def __str__(self):
+        return f'{self.user.id}'
