2d5b51c1c3fa896fd9fa44dd82d751e54c2efe28
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2024-01-17 22:32:48 +0100

    latest commit

--- users/components/update_profile.py
+++ users/components/update_profile.py
@@ -4,11 +4,18 @@ from users.models import Profile, User
 class UpdateProfileView(UnicornView):
     profile: Profile = None
     user: User = None
+    msg:str = ""
+    cls:str = ""
     def mount(self):
         self.profile =self.request.user.user_profile#  Profile.objects.filter(user=self.request.user).first()
         self.user = self.request.user
 
+    def message(self, cls, msg):
+        self.msg = msg
+        self.cls = cls
+        self.call('success')
+        
     def save(self):
         self.profile.save()
         self.user.save()
-        self.call('success')
+        self.message('success', "Profile updated successfully!")
